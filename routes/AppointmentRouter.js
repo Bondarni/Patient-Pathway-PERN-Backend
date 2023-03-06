0b923ff1ddda953ac61244a2b0a0a25e2ba1df1d
Joshua Jumelet <joshuajumelet@gmail.com>
2023-03-06 06:23:40 -0800

    build middleware folder & .gitignore .env files

--- routes/AppointmentRouter.js
+++ routes/AppointmentRouter.js
@@ -1,11 +1,11 @@
 const Router = require('express').Router()
 const controller = require('../controllers/AppointmentController')
 
+Router.get('/', controller.GetAppointments)
+Router.get('/:appointment_id', controller.GetAppointmentDetails)
 Router.post(
   '/patient/:patient_id/doctor/:doctor_id',
   controller.CreateAppointment
 )
-Router.get('/', controller.GetAppointments)
-Router.get('/details/:appointment_id', controller.GetAppointmentDetails)
 
 module.exports = Router
